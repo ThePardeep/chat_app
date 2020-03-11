0a5487de2a8a6a1727f4173218901d73308c0e24
unknown <pardeepsaini7927@gmail.com>
2020-03-11 13:13:18 +0530

    com

--- client/src/Components/Socket/Socket.js
+++ client/src/Components/Socket/Socket.js
@@ -2,8 +2,7 @@ import io from "socket.io-client";
 
 const PORT = process.env.PORT || 7000;
 //sheltered-dusk-68201.herokuapp.com/
-
-https: const Socket = User => {
+ const Socket = User => {
   const socket = io(`https://sheltered-dusk-68201.herokuapp.com:${PORT}`);
   return socket;
 };
